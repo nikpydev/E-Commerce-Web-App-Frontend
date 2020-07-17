45b75627b2e372c6de4202805f5c26c3b2ae990c
Nikhil Choudhary <nc1336282@gmail.com>
2020-07-17 18:28:22 +0530

    Updates

---
+++ precache-manifest.49094607b82afa5d2571e7263c1901c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad8150da175a3f3652b190c60619ed0",
+    "url": "/Shoppersonic-Frontend/index.html"
+  },
+  {
+    "revision": "980a47679accedabb6c4",
+    "url": "/Shoppersonic-Frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f56ae9d5169b3becaf31",
+    "url": "/Shoppersonic-Frontend/static/css/main.c7bf5bc0.chunk.css"
+  },
+  {
+    "revision": "980a47679accedabb6c4",
+    "url": "/Shoppersonic-Frontend/static/js/2.e1915a48.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Shoppersonic-Frontend/static/js/2.e1915a48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56ae9d5169b3becaf31",
+    "url": "/Shoppersonic-Frontend/static/js/main.739af906.chunk.js"
+  },
+  {
+    "revision": "7429d6f75627ca4888e7",
+    "url": "/Shoppersonic-Frontend/static/js/runtime-main.637dfa55.js"
+  }
+]);
